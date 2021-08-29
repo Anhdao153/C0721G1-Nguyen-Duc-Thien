42e24a0d71518c75597b395dba6c4c65f2d2bfd9
Anhdao153 <ndthien.1532000@gmai.com>
2021-08-29 17:54:01 +0700

    DSA

---
+++ Module2/Main_project/src/list/bai_tap/arraylist/MylistJVtest.java
@@ -0,0 +1,26 @@
+package list.bai_tap.arraylist;
+
+public class MylistJVtest {
+
+        public static void main(String[] args) {
+            MyList<Integer> myList = new MyList<Integer>();
+            myList.add(0, 1);
+            myList.add(1, 2);
+            myList.add(2, 3);
+            myList.add(3, 4);
+            myList.remove(1);
+            myList.remove(2);
+            System.out.println(myList.getSize());
+            System.out.println(myList.get(1));
+            System.out.println(myList.contains(3));
+            System.out.println(myList.indexOf(3));
+            System.out.println(myList.add(3));
+            MyList<Integer> myClone = myList.clone();
+            myClone.add(0, 1);
+            myClone.add(1, 2);
+            myClone.add(2, 3);
+            myClone.add(3, 4);
+            System.out.println(myClone.get(2));
+        }
+
+}
---
+++ Module2/Main_project/src/list/thuc_hanh/TestMyLinkedLis.java
@@ -0,0 +1,18 @@
+package list.thuc_hanh;
+
+public class TestMyLinkedLis {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.get(3);
+        ll.add(10,34);
+        ll.addFirst(2);
+        ll.printList();
+    }
+}
